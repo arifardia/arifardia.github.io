55c7ff59d975d60ee93a8f78cc11e4d33b8defec
arifardia <arifard49@gmail.com>
2025-03-19 09:40:57 +0700

    up

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-// function darkMode() {
-//     let body = document.body;
-//     body.classList.toggle("dark-mode");
-
-// }
-
-function gotolib() {
-    const userConfirmation = confirm("Apakah Anda yakin ingin membuka halaman perpustakaan?");
-
-    if(userConfirmation) {
-        window.open('https://library.usu.ac.id/', '_blank');   
-    }
-}
+Swal.fire({
+    title: "Yo!",
+    text: "Welcome wonderer",
+    icon: "info",
+    confirmButtonText: "Mulai Jelajah",
+  });
+  
+  function gotolib() {
+    Swal.fire({
+      title: "Kunjungi Halaman Perpustakaan USU?",
+      text: "Anda akan diarahkan ke halaman web perpustakaan.",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Ya, Lanjutkan!",
+      cancelButtonText: "Batal",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Mengalihkan...",
+          text: "Mohon tunggu sebentar.",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 1500, // Tutup otomatis setelah 1.5 detik
+        }).then(() => {
+          window.open("https://library.usu.ac.id/", "_blank");
+        });
+      }
+    });
+  }
