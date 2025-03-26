25129e3f912cd3a3ce7913694cd4e830e0b04009
arifardia <arifard49@gmail.com>
2025-03-26 14:32:42 +0700

    add DOM

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// Swal.fire({
-//     title: "Yo!",
-//     text: "Welcome wonderer",
-//     icon: "info",
-//     confirmButtonText: "Mulai Jelajah",
-//   });
-  
-  function gotolib() {
-    Swal.fire({
-      title: "Kunjungi Halaman Perpustakaan USU?",
-      text: "Anda akan diarahkan ke halaman web perpustakaan.",
-      icon: "question",
-      showCancelButton: true,
-      confirmButtonText: "Ya, Lanjutkan!",
-      cancelButtonText: "Batal",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Mengalihkan...",
-          text: "Mohon tunggu sebentar.",
-          icon: "success",
-          showConfirmButton: false,
-          timer: 1500, // Tutup otomatis setelah 1.5 detik
-        }).then(() => {
-          window.open("https://library.usu.ac.id/", "_blank");
-        });
-      }
-    });
-  }
+// Tambahin DOM
+let buttonGoToLib = document.getElementById("libraryButton");
+buttonGoToLib.addEventListener("click", gotolib);
+
+function gotolib() {
+  Swal.fire({
+    title: "Kunjungi Halaman Perpustakaan USU?",
+    text: "Anda akan diarahkan ke halaman web perpustakaan.",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Ya, Lanjutkan!",
+    cancelButtonText: "Batal",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Mengalihkan...",
+        text: "Mohon tunggu sebentar.",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500, // Tutup otomatis setelah 1.5 detik
+      }).then(() => {
+        window.open("https://library.usu.ac.id/", "_blank");
+      });
+    }
+  });
+}
 
 
